788d933a265c71869bb8a82ce4608830463cccc9
enginesa <engin1218@hotmail.com>
2020-08-09 22:59:17 +0300

    store apikey

---
+++ src/store/store.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        apikey: "AIzaSyAXhrsyMdcO4ZvED0fcn1yiLTAXhkATBpE"
+    },
+    getters:{
+
+    },
+    mutations: {
+
+    },
+    actions:{
+
+    }
+})
+
+
+export default store;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -34,3 +34,17 @@
 
 </template>
 
+<script>
+    // import axios from 'axios';
+    export default {
+        created() {
+            // axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAXhrsyMdcO4ZvED0fcn1yiLTAXhkATBpE',
+            //     {email:"test@hotmai1l.com",password:"1218620",returnSecureToken:true})
+            //     .then(function (response) {
+            //         // handle success
+            //         console.log(response);
+            //     })
+
+        }
+    }
+</script>
