49f263348a9b256f200999b2aa9c8382b0a9fc00
enginesa <engin1218@hotmail.com>
2020-08-18 22:54:04 +0300

    son

--- src/components/pages/Home/Level.vue
+++ src/components/pages/Home/Level.vue
@@ -2,26 +2,26 @@
     <nav class="level levelHome">
         <div class="level-item has-text-centered">
             <div>
-                <p class="heading">Tweets</p>
-                <p class="title">3,456</p>
+                <p class="heading">Üye</p>
+                <p class="title">2</p>
             </div>
         </div>
         <div class="level-item has-text-centered">
             <div>
-                <p class="heading">Following</p>
+                <p class="heading">Not</p>
                 <p class="title">123</p>
             </div>
         </div>
         <div class="level-item has-text-centered">
             <div>
-                <p class="heading">Followers</p>
-                <p class="title">456K</p>
+                <p class="heading">Silinen</p>
+                <p class="title">345</p>
             </div>
         </div>
         <div class="level-item has-text-centered">
             <div>
-                <p class="heading">Likes</p>
-                <p class="title">789</p>
+                <p class="heading">Düzenlenen</p>
+                <p class="title">678</p>
             </div>
         </div>
     </nav>
--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -2,7 +2,7 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
             <router-link :to='{name:"Home"}' class="navbar-item">
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+                <img src="../../assets/logo.png">
             </router-link>
             <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
                data-target="navbarBasicExample">
@@ -16,6 +16,7 @@
                 <div class="navbar-item">
                     <div class="buttons">
                         <router-link :to='{name:"NewNote"}' class="button is-danger" v-if="authControl"><strong>Yeni not</strong></router-link>
+                        <div class="button is-light" v-if="authControl" @click="logOut"><strong>Çıkış yap</strong></div>
                         <router-link :to='{name:"Register"}' class="button is-primary" v-if="!authControl"><strong>Kayıt ol</strong></router-link>
                         <router-link :to='{name:"Login"}' class="button is-light" v-if="!authControl">Giriş yap</router-link>
                     </div>
@@ -41,7 +42,15 @@
             authControl(){
                 return this.$store.getters.getToken;
             }
-       }
+       },
+        methods:{
+            logOut(){
+                this.$store.dispatch("logOut");
+                setTimeout( ()=>{
+                    this.$router.push({name:"Home"})
+                })
+            }
+        }
     }
 </script>
 
